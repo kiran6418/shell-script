9ae88f82e000c675170a7834e2c45dc053157eeb
kiran <kiranbabukolusu@gmail.com>
2025-01-01 18:13:02 +0530

    conversation added to the script

---
+++ 01-kiran1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=RAMESH
+PEROSN2=SURESH
+
+
+echo "$PERSON1:: hi $PERSON2"
+echo "$PERSON2 :: hi $PERSON1"
+echo "$PERSON2 :: how are you"
+echo "$PERSON1 :: i am good thanks"
+echo "$PERSON2 :: i am also good thnaks"
